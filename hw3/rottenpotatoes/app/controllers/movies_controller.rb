f8b506f9afd177ea3dc72aa49ade460ce77d0019
Michael Lockette <michael@UbuntuVirtual.(none)>
2014-02-13 21:54:39 -0700

    Thursday Night after PeakChaos almost done with Part One from laptop

--- hw3/rottenpotatoes/app/controllers/movies_controller.rb
+++ hw3/rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,8 +7,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all 
+    sort = params[:sort] || session[:sort]
+    case sort
+      when 'title'
+        ordering,@title_header = {:order => :title}, 'hilite'
+      when 'release_date'
+        ordering,@date_header = {:order => :release_date}, 'hilite'
+      end
+
+      if params[:sort] != session[:sort]
+      session[:sort] = sort
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+
+    #@all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
+ 
   end
+ 
 
   def new
     # default: render 'new' template
