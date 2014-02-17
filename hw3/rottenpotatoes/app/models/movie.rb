b45c0f0193e84b46e0e8905af0d780456f9e15f6
Michael Lockette <michael@TantiveIV.(none)>
2014-02-17 10:37:15 -0700

    Monday morning Part One plus more working on hw this was from TantiveIV

--- hw3/rottenpotatoes/app/models/movie.rb
+++ hw3/rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+=begin	
+	def Movie.get_all_ratings
+		all_ratings = []
+		self.select(:rating).group(:rating).each do |movie|
+			all_ratings << movie.rating
+		end
+		return all_ratings
+	end
+=end
+	def self.all_ratings
+    	%w(G PG PG-13 NC-17 R)
+  	end
 end
