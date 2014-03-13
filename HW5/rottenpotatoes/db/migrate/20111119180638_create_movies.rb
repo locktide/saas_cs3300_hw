4aa078c0c58f31abf082867e29479f0e8427c8c2
Michael Lockette <michael@UbuntuVirtual.(none)>
2014-03-13 13:45:32 -0600

    Start of HW5 in CAP"S Thursday during SAAS maybe have part 1 completed

---
+++ HW5/rottenpotatoes/db/migrate/20111119180638_create_movies.rb
@@ -0,0 +1,19 @@
+class CreateMovies < ActiveRecord::Migration
+  def up
+    create_table :movies do |t|
+      t.string :title
+      t.string :rating
+      t.text :description
+      t.datetime :release_date
+      #add the director field
+      t.string :director
+      # Add fields that let Rails automatically keep track
+      # of when movies are added or modified:
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :movies
+  end
+end
